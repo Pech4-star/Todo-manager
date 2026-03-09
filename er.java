f6e4ba09aac3b1e0db9f0c340db2bdfec1695fef
Pech4-star <pheng11@paragoniu.edu.kh>
2026-03-09 14:50:06 +0700

    Initial Spring Boot project

---
+++ er.java
@@ -0,0 +1,11 @@
+[33mcommit 8e096cc6e5a74734491b2ee6127341655c12d537[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m)[m
+Author: Pech4-star <pheng11@paragoniu.edu.kh>
+Date:   Mon Mar 9 14:41:56 2026 +0700
+
+    Add User entity
+
+[33mcommit 65db49209ec527dd26b35c1583379e5193307a70[m
+Author: Pech4-star <pheng11@paragoniu.edu.kh>
+Date:   Mon Mar 9 14:31:07 2026 +0700
+
+    Initial Spring Boot project
